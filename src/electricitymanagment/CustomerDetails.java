2c61e2febce0f8c82883eca33a0f1d8c70a96ccc
Yash Agarwal <144467623+YashAgarwal28@users.noreply.github.com>
2024-02-25 20:55:15 +0530

    Create CustomerDetails.java

---
+++ src/electricitymanagment/CustomerDetails.java
@@ -0,0 +1,57 @@
+package electricitymanagment;
+
+import java.awt.*;
+import javax.swing.*;
+import java.sql.*;
+import net.proteanit.sql.DbUtils;
+import java.awt.event.*;
+import java.awt.print.PrinterException;
+
+public class CustomerDetails extends JFrame implements ActionListener{
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	JTable table;
+    JButton print;
+    
+    CustomerDetails(){
+        super("Customer Details");
+        
+        setSize(700, 500);
+        setLocation(200, 150);
+        
+        table = new JTable();
+        table.setBackground(new Color(0X78DEC7));
+        try {
+            conn c = new conn();
+            ResultSet rs = c.s.executeQuery("select * from customer");
+            
+            table.setModel(DbUtils.resultSetToTableModel(rs));
+        } catch (Exception e) {
+        }
+        
+        JScrollPane sp = new JScrollPane(table);
+        add(sp);
+        
+        print = new JButton("Print");
+        print.addActionListener(this);
+        print.setBackground(new Color(0X78DEC7));
+        add(print, "South");
+        
+        
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        try {
+            table.print();
+        } catch (PrinterException e) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new CustomerDetails();
+    }
+}
